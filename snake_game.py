d7b88f7289e5fa28059c8ba14234c1b93138a618
alphanoobie <hussaingadi.hg@gmail.com>
2023-07-07 00:30:57 +0530

    collect user input

--- snake_game.py
+++ snake_game.py
@@ -63,6 +63,15 @@ class SnakeGame:
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    self.direction = Direction.LEFT
+                elif event.key == pygame.K_RIGHT:
+                    self.direction = Direction.RIGHT
+                elif event.key == pygame.K_UP:
+                    self.direction = Direction.UP
+                elif event.key == pygame.K_DOWN:
+                    self.direction = Direction.DOWN            
         # update ui
         self._update_ui()
         self.clock.tick(SPEED)
