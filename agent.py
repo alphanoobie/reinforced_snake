10b3a68f55d5e7f8d578365f0dd0b8a856fbc2f0
alphanoobie <hussaingadi.hg@gmail.com>
2023-07-16 20:23:19 +0530

    training long and short memory

--- agent.py
+++ agent.py
@@ -15,6 +15,8 @@ class Agent:
         self.epsilon = 0  # randomness
         self.gamma = 0  # discount
         self.memory = deque(maxlen=MAX_MEMORY)
+        self.model = None
+        self.trainer = None
         # model, trainer
 
     def get_state(self, game):
@@ -60,13 +62,19 @@ class Agent:
         return np.array(state, dtype=int)
 
     def remember(self, state, action, reward, next_state, done):
-        pass
+        self.memory.append((state, action, reward, next_state, done))
 
     def train_long_memory(self):
-        pass
+        if len(self.memory) > BATCH_SIZE:
+            mini_sample = random.sample(self.memory, BATCH_SIZE)
+        else:
+            mini_sample = self.memory
+
+        states, actions, rewards, next_states, dones = zip(*mini_sample)
+        self.trainer.train_step(states, actions, rewards, next_states, dones)
 
     def train_short_memory(self, state, action, reward, next_state, done):
-        pass
+        self.trainer.train_step(state, action, reward, next_state, done)
 
     def get_action(self, state):
         pass
