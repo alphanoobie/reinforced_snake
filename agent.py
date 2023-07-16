033f2c8c7ce6daf7bfcaf540c4773d409f3f630d
alphanoobie <hussaingadi.hg@gmail.com>
2023-07-16 20:14:19 +0530

    get state func

--- agent.py
+++ agent.py
@@ -18,7 +18,46 @@ class Agent:
         # model, trainer
 
     def get_state(self, game):
-        pass
+        head = game.snake[0]
+        point_l = Point(head.x - 20, head.y)
+        point_r = Point(head.x + 20, head.y)
+        point_u = Point(head.x, head.y - 20)
+        point_d = Point(head.x, head.y + 20)
+
+        dir_l = game.direction == Direction.LEFT
+        dir_r = game.direction == Direction.RIGHT
+        dir_u = game.direction == Direction.UP
+        dir_d = game.direction == Direction.DOWN
+
+        state = [
+            # Danger straight
+            (dir_r and game.is_collision(point_r))
+            or (dir_l and game.is_collision(point_l))
+            or (dir_u and game.is_collision(point_u))
+            or (dir_d and game.is_collision(point_d)),
+            # Danger right
+            (dir_u and game.is_collision(point_r))
+            or (dir_d and game.is_collision(point_l))
+            or (dir_l and game.is_collision(point_u))
+            or (dir_r and game.is_collision(point_d)),
+            # Danger left
+            (dir_d and game.is_collision(point_r))
+            or (dir_u and game.is_collision(point_l))
+            or (dir_r and game.is_collision(point_u))
+            or (dir_l and game.is_collision(point_d)),
+            # Move direction
+            dir_l,
+            dir_r,
+            dir_u,
+            dir_d,
+            # Food location
+            game.food.x < game.head.x,  # food left
+            game.food.x > game.head.x,  # food right
+            game.food.y < game.head.y,  # food up
+            game.food.y > game.head.y,  # food down
+        ]
+
+        return np.array(state, dtype=int)
 
     def remember(self, state, action, reward, next_state, done):
         pass
@@ -51,26 +90,25 @@ def train():
         reward, done, score = game.play_step(final_move)
         state_new = agent.get_state(game)
 
-        #train short memory
+        # train short memory
         agent.train_short_memory(state_old, final_move, reward, state_new, done)
 
-        #remember
+        # remember
         agent.remember(state_old, final_move, reward, state_new, done)
 
         if done:
-            #train long memory
+            # train long memory
             game._reset()
-            agent.n_games +=1
+            agent.n_games += 1
             agent.train_long_memory
 
             if score > record:
                 record = score
-                #agent.model.save
+                # agent.model.save
 
-            print('Game:', agent.n_games, 'Score:', score, 'Record:', record)
+            print("Game:", agent.n_games, "Score:", score, "Record:", record)
 
-            #plot
-            
+            # plot
 
 
 if __name__ == "__main__":
