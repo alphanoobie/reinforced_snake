d284de1a73a81be694aa2735faf52811551f84b3
alphanoobie <hussaingadi.hg@gmail.com>
2023-07-16 21:32:47 +0530

    tranining step function

--- model.py
+++ model.py
@@ -23,6 +23,7 @@ class Linear_QNet(nn.Module):
         file_name = os.path.join(model_folder_path, file_name)
         torch.save(self.state_dict(), file_name)
 
+
 class QTrainer:
     def __init__(self, model, lr, gamma):
         self.lr = lr
@@ -31,6 +32,36 @@ class QTrainer:
         self.optimizer = optim.Adam(model.parameters(), lr=self.lr)
         self.criterion = nn.MSELoss()
 
-    def train_step(self, state, final_move, reward, next_state, done):
-        pass
+    def train_step(self, state, action, reward, next_state, done):
+        state = torch.tensor(state, dtype=torch.float)
+        next_state = torch.tensor(next_state, dtype=torch.float)
+        action = torch.tensor(action, dtype=torch.long)
+        reward = torch.tensor(reward, dtype=torch.float)
 
+        if len(state.shape) == 1:
+            state = torch.unsqueeze(state, 0)
+            next_state = torch.unsqueeze(next_state, 0)
+            action = torch.unsqueeze(action, 0)
+            reward = torch.unsqueeze(reward, 0)
+            done = (done,)
+
+        # predicted Q values with current state
+        pred = self.model(state)
+
+        target = pred.clone()
+        for index in range(len(done)):
+            Q_new = reward[index]
+            if not done[index]:
+                Q_new = reward[index] + self.gamma * torch.max(
+                    self.model(next_state[index])
+                )
+
+            target[index][torch.argmax(action).item()] = Q_new
+
+        # Q_new = r + y * max(next_predicted q value)
+
+        self.optimizer.zero_grad()
+        loss = self.criterion(target, pred)
+        loss.backward()
+
+        self.optimizer.step()
